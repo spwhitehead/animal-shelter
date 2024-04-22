a001cac5a37c0fb100c804fbc64dda93f112aeb4
Greg Anderson <greg.anderson.cs@gmail.com>
2024-04-22 15:16:55 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+shelters: list = [
+    {
+        "name": "St. George Animal Shelter",
+        "address": "605 Waterworks Dr, St. George, UT 84770",
+        "animals": {
+            "cats": 13,
+            "dogs": 15,
+        }
+    },
+    {
+        "name": "St. George Paws",
+        "address": "1125 W 1130 N, St. George, UT 84770",
+        "animals": {
+            "cats": 12,
+            "dogs": 9,
+        }
+    },
+    {
+        "name": "Animal Rescue Team",
+        "address": "1838 W 1020 N Ste. B, St. George, UT 84770",
+        "animals": {
+            "cats": 4,
+            "dogs": 7,
+        }
+    }
+]
